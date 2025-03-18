d6ccc783752e06aa68cc6cd0c67e872b87ea5e5a
Quinol Jeremy <jeremy.quinol@gmail.com>
2025-03-18 16:45:09 +0100

    ajout entity user

--- src/main/java/com/psavi/core/entity/User.java
+++ src/main/java/com/psavi/core/entity/User.java
@@ -1,4 +1,49 @@
 package com.psavi.core.entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int ID;
+    @Column
+    private String nom;
+    @Column
+    private String prenom;
+    @Column
+    private String email;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
